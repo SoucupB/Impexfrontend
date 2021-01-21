0d7ec83a84ad64b52387e496b447c249881a7152
Bogdan Soucup <soucup.bogdan@gmail.com>
2021-01-21 22:51:45 +0200

    Added email rotator simple rotator

--- scripts/Mailer.js
+++ scripts/Mailer.js
@@ -1,12 +1,30 @@
 function sdEmail(email, desc) {
+  waitingState()
   $.ajax({
     type: "GET",
     url: 'http://' + publicIP + ':' + publicPort + '/sendEmail?email=' + email + "&description=" + desc,
     data: {},
     success: function( data ) {
       console.log("Email sent");
+      $('#star_rotation').remove()
+      createHtml()
     },
     dataType: 'json'
   });
   return false;
+}
+
+function insertAfter(newNode, referenceNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function waitingState() {
+  $("#success_msg").remove()
+  $("#email_div").append("<div id = 'star_rotation' class = 'star'>★</div>")
+}
+
+function createHtml() {
+  $('#star_rotation').remove()
+  $("#success_msg").remove()
+  $("#email_div").append("<span id = 'success_msg' style='position: relative; top: -20px; color:green;'>Your email has been sent! ✓</span>");
 }
